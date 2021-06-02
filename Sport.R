5bf12524f8603be6dbb7df5d562293310bcf43a2
saacregar <s.crecente@gmail.com>
2021-06-02 14:25:10 +0200

    Añado lo hecho en la práctica: importar, cambiar nombre columna Date

--- Sport.R
+++ Sport.R
@@ -1,3 +1,31 @@
-install.packages("ggplot2")
-library(ggplot2)
-as.data.frame(as.formula())
+##=========================================================================##
+##                                                                         ##
+##                DIGITALIZACIÓN DEL DEPORTE                               ##
+##              Sabela Crecente y Albert Ortiz                             ##
+##                                                                         ##
+##=========================================================================##
+
+# #### GESTIÓN DE PAQUETES ####
+
+if(!require("tidyverse")) {
+  install.packages("tidyverse")
+  library("tidyverse")
+}
+
+if(!require("dplyr")) {
+  install.packages("dplyr")
+  library("dplyr")
+}
+
+
+# #### IMPORTACIÓN DE DATOS ####
+setwd(getwd()) #CONFIRMAR Q ASI ESTA BIEN
+#Datos extraídos de https://old.datahub.io/dataset/uk-premier-league-match-by-match-2011-2012
+#esportados a csv desde Excel
+
+dfUK <- read.csv("Premier League 2011-12 Match by Match.csv", header=TRUE, sep=";", skip = 0, na.strings = "NA")
+
+str(dfUK)
+
+dfUK<-dfUK %>% rename("Date"="ï..Date")
+
