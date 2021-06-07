3bb9406bee33a3491e6091ad866f8cc77e324644
saacregar <s.crecente@gmail.com>
2021-06-07 08:32:03 +0200

    a<sdz

--- Sport.R
+++ Sport.R
@@ -21,11 +21,15 @@ if(!require("dplyr")) {
 # #### IMPORTACIÓN DE DATOS ####
 setwd(getwd()) #CONFIRMAR Q ASI ESTA BIEN
 #Datos extraídos de https://old.datahub.io/dataset/uk-premier-league-match-by-match-2011-2012
-#esportados a csv desde Excel
+#exportados a csv desde Excel
 
-dfUK <- read.csv("Premier League 2011-12 Match by Match.csv", header=TRUE, sep=";", skip = 0, na.strings = "NA")
+df <- read.csv("Premier League 2011-12 Match by Match.csv", header=TRUE, sep=";", skip = 0, na.strings = "NA")
 
-str(dfUK)
+str(df)
 
-dfUK<-dfUK %>% rename("Date"="ï..Date")
+df<-df %>% rename("Date"="ï..Date")
 
+#Unir nombre y apellido de los jugadores
+df$Player.Name<-paste(df$Player.Forename,df$Player.Surname)
+df <- cbind(df, df$Player.Name)
+Data<-write.table(df)
